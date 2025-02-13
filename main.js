5e04afd49fd24b1bc078b64326c320ec25c57aaa
Andrea Senni <andreasenni8@gmail.com>
2025-02-13 19:31:18 +0100

    Def(?)

--- main.js
+++ main.js
@@ -2,39 +2,33 @@ let greetings = document.getElementById("greetings");
 
 let myselfSection = document.getElementById("myselfSection");
 let myselfLogo = document.getElementById("myselfLogo");
-myselfSection.addEventListener("mouseenter", function () {
-  myselfLogo.classList.add("focus");
-  myselfLogo.classList.add("gira");
+myselfSection.addEventListener("pointerenter", function () {
+  myselfLogo.classList.add("focus", "gira");
   greetings.style.opacity = "0";
 });
-myselfSection.addEventListener("mouseleave", function () {
-  myselfLogo.classList.remove("focus");
-  myselfLogo.classList.remove("gira");
+myselfSection.addEventListener("pointerleave", function () {
+  myselfLogo.classList.remove("focus", "gira");
   greetings.style.opacity = "1";
 });
 
 let workSection = document.getElementById("workSection");
 let workLogo = document.getElementById("workLogo");
-workSection.addEventListener("mouseenter", function () {
-  workLogo.classList.add("focus");
-  workLogo.classList.add("gira");
+workSection.addEventListener("pointerenter", function () {
+  workLogo.classList.add("focus", "gira");
   greetings.style.opacity = "0";
 });
-workSection.addEventListener("mouseleave", function () {
-  workLogo.classList.remove("focus");
-  workLogo.classList.remove("gira");
+workSection.addEventListener("pointerleave", function () {
+  workLogo.classList.remove("focus", "gira");
   greetings.style.opacity = "1";
 });
 
 let skillsSection = document.getElementById("skillsSection");
 let skillsLogo = document.getElementById("skillsLogo");
-skillsSection.addEventListener("mouseenter", function () {
-  skillsLogo.classList.add("focus");
-  skillsLogo.classList.add("gira");
+skillsSection.addEventListener("pointerenter", function () {
+  skillsLogo.classList.add("focus", "gira");
   greetings.style.opacity = "0";
 });
-skillsSection.addEventListener("mouseleave", function () {
-  skillsLogo.classList.remove("focus");
-  skillsLogo.classList.remove("gira");
+skillsSection.addEventListener("pointerleave", function () {
+  skillsLogo.classList.remove("focus", "gira");
   greetings.style.opacity = "1";
 });
